4c669ecd69b77f6ca66903adc1a17d70a4a12c03
qianlima8888 <1058088476@qq.com>
2024-12-04 19:19:10 +0800

    完善删除不符合条件节点的代码

--- processProxy.py
+++ processProxy.py
@@ -1,4 +1,4 @@
-def removeDuplicateNode(proxyPool):
+def removeDuplicateNode(proxyPool): #删除重复节点
     checkLists = ["name", "server"]
 
     allProxy = proxyPool
@@ -6,28 +6,31 @@ def removeDuplicateNode(proxyPool):
         proxiesItem  = []
         proxies = []
         for proxy in allProxy:
-            if(proxy[item] not in proxiesItem):
-                proxies.append(proxy)
-                proxiesItem.append(proxy[item])
+            if(proxy[item] in proxiesItem):
+                continue
+            proxies.append(proxy)
+            proxiesItem.append(proxy[item])
         allProxy = proxies
 
     return allProxy
 
-def removeNotSupportCipher(proxyPool):
+def removeNotSupportCipher(proxyPool): #删除cipher不符合条件的节点
     notSupportCipher = ['ss']
     proxies = []
     for proxy in proxyPool:
-        if('cipher' in proxy and proxy['cipher'] not in notSupportCipher):
-            proxies.append(proxy)
+        if('cipher' in proxy and proxy['cipher'] in notSupportCipher):
+            continue
+        proxies.append(proxy)
 
     return proxies
 
-def removeNotSupportUUID(proxyPool):
+def removeNotSupportUUID(proxyPool): #删除uuid不符合条件的节点
     notSupportUUID = ['Free']
     proxies = []
     for proxy in proxyPool:
-        if('uuid' in proxy and proxy['uuid'] not in notSupportUUID):
-            proxies.append(proxy)
+        if('uuid' in proxy and proxy['uuid'] in notSupportUUID):
+            continue
+        proxies.append(proxy)
 
     return proxies
 
