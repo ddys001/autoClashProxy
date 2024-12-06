29a91e1739305b8560895b392126447f96681be5
qianlima8888 <1058088476@qq.com>
2024-12-06 22:23:05 +0800

    完善代码

--- autoPush.py
+++ autoPush.py
@@ -8,7 +8,10 @@ def pushListFile(listFile):
         print("本地订阅文件已更新，开始推送至github。")
         repo.git.add(listFile)
         repo.git.commit("-m", "更新节点")
-        repo.remotes.origin.push()
-        print("订阅文件已在github上更新")
+        try:
+            repo.remotes.origin.push()
+            print("订阅文件已在github上更新")
+        except git.exc.GitCommandError:
+            print("代码推送至github失败")
     else:
         print("本地订阅文件未更新，无需推送至github。")
